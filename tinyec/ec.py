c15a57139cf3db99f8bc105daee8450c6b91d357
Eli Ribble <eli@theribbles.org>
2023-11-08 14:36:02 -0700

    Add initial types to function signatures.
    We still have a variety of typing errors, but this at least gives us something
    to target based on the behavior of the unit tests.

--- tinyec/ec.py
+++ tinyec/ec.py
@@ -1,8 +1,9 @@
 import random
+from typing import Any, Optional, Tuple, Union
 
 import warnings
 
-def egcd(a, b):
+def egcd(a: int, b: int) -> Tuple[int, int, int]:
     if a == 0:
         return b, 0, 1
     else:
@@ -10,7 +11,7 @@ def egcd(a, b):
         return g, x - (b // a) * y, y
 
 
-def mod_inv(a, p):
+def mod_inv(a: int, p: int) -> int:
     if a < 0:
         return p - mod_inv(-a, p)
     g, x, y = egcd(a, p)
@@ -21,69 +22,69 @@ def mod_inv(a, p):
 
 
 class SubGroup:
-    def __init__(self, p, g, n, h):
+    def __init__(self, p: int, g: Tuple[int, int], n: int, h:int):
         self.p = p
         self.g = g
         self.n = n
         self.h = h
 
-    def __eq__(self, other):
+    def __eq__(self, other: Any) -> bool:
         if not isinstance(other, SubGroup):
             return False
         return self.p == other.p and self.g == other.g and self.n == other.n and self.h == other.h
 
-    def __ne__(self, other):
+    def __ne__(self, other: Any) -> bool:
         return not self.__eq__(other)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return "Subgroup => generator %s, order: %d, cofactor: %d on Field => prime %d" % (self.g, self.n,
                                                                                            self.h, self.p)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return self.__str__()
 
 
 class Curve:
-    def __init__(self, a, b, field, name="undefined"):
+    def __init__(self, a: int, b: int, field: SubGroup, name:str="undefined"):
         self.name = name
         self.a = a
         self.b = b
         self.field = field
         self.g = Point(self, self.field.g[0], self.field.g[1])
 
-    def is_singular(self):
+    def is_singular(self) -> bool:
         return (4 * self.a**3 + 27 * self.b**2) % self.field.p == 0
 
-    def on_curve(self, x, y):
+    def on_curve(self, x: int, y: int) -> bool:
         return (y**2 - x**3 - self.a * x - self.b) % self.field.p == 0
 
-    def __eq__(self, other):
+    def __eq__(self, other: Any) -> bool:
         if not isinstance(other, Curve):
             return False
         return self.a == other.a and self.b == other.b and self.field == other.field
 
-    def __ne__(self, other):
+    def __ne__(self, other: Any) -> bool:
         return not self.__eq__(other)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return "\"%s\" => y^2 = x^3 + %dx + %d (mod %d)" % (self.name, self.a, self.b, self.field.p)
 
 
 class Inf:
-    def __init__(self, curve, x=None, y=None):
+    def __init__(self, curve: Curve, x: Optional[int]=None, y: Optional[int]=None):
         self.x = x
         self.y = y
         self.curve = curve
 
-    def __eq__(self, other):
+    def __eq__(self, other: Any) -> bool:
         if not isinstance(other, Inf):
             return False
         return self.curve == other.curve
 
-    def __ne__(self, other):
+    def __ne__(self, other: Any) -> bool:
         return not self.__eq__(other)
 
-    def __add__(self, other):
+    def __add__(self, other: Any) -> Union["Inf", "Point"]:
         if isinstance(other, Inf):
             return Inf()
         if isinstance(other, Point):
@@ -91,7 +92,7 @@ class Inf:
         raise TypeError("Unsupported operand type(s) for +: '%s' and '%s'" % (other.__class__.__name__,
                                                                                   self.__class__.__name__))
 
-    def __sub__(self, other):
+    def __sub__(self, other: Any) -> Union["Inf", "Point"]:
         if isinstance(other, Inf):
             return Inf()
         if isinstance(other, Point):
@@ -99,15 +100,15 @@ class Inf:
         raise TypeError("Unsupported operand type(s) for +: '%s' and '%s'" % (other.__class__.__name__,
                                                                                   self.__class__.__name__))
 
-    def __str__(self):
+    def __str__(self) -> str:
         return "%s on %s" % (self.__class__.__name__, self.curve)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return self.__str__()
 
 
 class Point:
-    def __init__(self, curve, x, y):
+    def __init__(self, curve: Curve, x: int, y: int):
         self.curve = curve
         self.x = x
         self.y = y
@@ -117,21 +118,21 @@ class Point:
             warnings.warn("Point (%d, %d) is not on curve \"%s\"" % (self.x, self.y, self.curve))
             self.on_curve = False
 
-    def __m(self, p, q):
+    def __m(self, p: "Point", q: "Point") -> int:
         if p.x == q.x:
             return (3 * p.x**2 + self.curve.a) * mod_inv(2 * p.y, self.p)
         else:
             return (p.y - q.y) * mod_inv(p.x - q.x, self.p)
 
-    def __eq__(self, other):
+    def __eq__(self, other: Any) -> bool:
         if not isinstance(other, Point):
             return False
         return self.x == other.x and self.y == other.y and self.curve == other.curve
 
-    def __ne__(self, other):
+    def __ne__(self, other: Any) -> bool:
         return not self.__eq__(other)
 
-    def __add__(self, other):
+    def __add__(self, other: Any) -> Union[Inf, "Point"]:
         if isinstance(other, Inf):
             return self
         if isinstance(other, Point):
@@ -148,7 +149,7 @@ class Point:
             raise TypeError("Unsupported operand type(s) for +: '%s' and '%s'" % (other.__class__.__name__,
                                                                                   self.__class__.__name__))
 
-    def __sub__(self, other):
+    def __sub__(self, other: Any) -> Union[Inf, "Point"]:
         if isinstance(other, Inf):
             return self.__add__(other)
         if isinstance(other, Point):
@@ -157,7 +158,7 @@ class Point:
             raise TypeError("Unsupported operand type(s) for -: '%s' and '%s'" % (other.__class__.__name__,
                                                                                   self.__class__.__name__))
 
-    def __mul__(self, other):
+    def __mul__(self, other: Any) -> Union[Inf, "Point"]:
         if isinstance(other, Inf):
             return Inf(self.curve)
         if isinstance(other, int):
@@ -178,18 +179,18 @@ class Point:
             raise TypeError("Unsupported operand type(s) for *: '%s' and '%s'" % (other.__class__.__name__,
                                                                                   self.__class__.__name__))
 
-    def __rmul__(self, other):
+    def __rmul__(self, other: Any) -> Union[Inf, "Point"]:
         return self.__mul__(other)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return "(%d, %d) %s %s" % (self.x, self.y, "on" if self.on_curve else "off", self.curve)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return self.__str__()
 
 
 class Keypair:
-    def __init__(self, curve, priv=None, pub=None):
+    def __init__(self, curve: Curve, priv: Optional[int]=None, pub: Optional[Point]=None):
         if priv is None and pub is None:
             raise ValueError("Private and/or public key must be provided")
         self.curve = curve
@@ -204,10 +205,10 @@ class Keypair:
 
 
 class ECDH:
-    def __init__(self, keypair):
+    def __init__(self, keypair: Keypair):
         self.keypair = keypair
 
-    def get_secret(self, keypair):
+    def get_secret(self, keypair: Keypair) -> Point:
         # Don;t check if both keypairs are on the same curve. Should raise a warning only
         if self.keypair.can_sign and keypair.can_encrypt:
             secret = self.keypair.priv * keypair.pub
@@ -218,7 +219,7 @@ class ECDH:
         return secret
 
 
-def make_keypair(curve):
+def make_keypair(curve: Curve) -> Keypair:
     priv = random.randint(1, curve.field.n)
     pub = priv * curve.g
     return Keypair(curve, priv, pub)
